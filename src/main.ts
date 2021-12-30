73bb640468a2c8cdc85e57888d2317f66f10a752
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2021-12-30 12:54:38 +0900

    Enable to handle get request

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,9 @@ function testGreeter() {
   const user = "Grant";
   Logger.log(greeter(user));
 }
+
+function doGet(_e: any) {
+  var output = ContentService.createTextOutput("hoge");
+  output.setMimeType(ContentService.MimeType.TEXT);
+  return output;
+}
